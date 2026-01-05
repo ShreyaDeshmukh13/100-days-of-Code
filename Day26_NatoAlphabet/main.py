8185306932ec64ab293f1c7176df2e7f5a536c0c
ShreyaDeshmukh13 <shreyadesh0103@gmail.com>
2026-01-05 07:42:54 +0530

    Add files via upload

---
+++ Day26_NatoAlphabet/main.py
@@ -0,0 +1,13 @@
+#TODO 1. Create a dictionary in this format:
+#{"A": "Alfa", "B": "Bravo"}
+
+import pandas as pd
+
+df = pd.read_csv('nato_phonetic_alphabet.csv')
+
+phonetic_dict = {row.letter:row.code for (index,row) in df.iterrows()}
+
+#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+word = input("Enter a word: ").upper()
+output_list = [phonetic_dict[letter] for letter in word]
+print(output_list)
